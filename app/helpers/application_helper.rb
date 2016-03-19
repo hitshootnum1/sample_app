976a4cf0b5326d7e7b486bf4018a89fa6bf875e3
hitshootnum1 <hitshootnum1@gmail.com>
2016-03-19 23:08:55 +0700

    Finish Sign Up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,7 @@ module ApplicationHelper
 	BASE_TITLE = 'Ruby on Rails Tutorial Sample App'
 
 	def full_title page_title = ''
-		if !page_title.empty?
-			page_title << ' | ' << BASE_TITLE
-		end
+		return page_title << ' | ' << BASE_TITLE unless page_title.empty?
 		BASE_TITLE
 	end
 end
