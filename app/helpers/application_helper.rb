5bbd03e2db2328d4a9bce594e2e2158dfd3309d0
hitshootnum1 <hitshootnum1@gmail.com>
2016-03-19 17:48:03 +0700

    Finish Static Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	BASE_TITLE = 'Ruby on Rails Tutorial Sample App'
+
+	def full_title page_title = ''
+		if !page_title.empty?
+			page_title << ' | ' << BASE_TITLE
+		end
+		BASE_TITLE
+	end
 end
