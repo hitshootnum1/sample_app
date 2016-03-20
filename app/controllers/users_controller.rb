444c6ad62cd5d9a3abae38f54427f8227d57f84d
hitshootnum1 <hitshootnum1@gmail.com>
2016-03-20 22:43:36 +0700

    Finish password-resets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   
-  before_action :logged_in_user, only: [:update, :edit]
+  before_action :logged_in_user, only: [:update, :edit, :destroy]
   before_action :correct_user, only: [:update, :edit]
+  before_action :admin_user, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page])
+    store_location
   end
 
   def new
@@ -12,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:id])
+  	@user = User.get_user_by_id(params[:id])
   end
 
   def create
@@ -21,13 +23,15 @@ class UsersController < ApplicationController
       log_in @user
   		flash[:success] = 'Welcom to Sample App!'
   		redirect_to root_url
+      activate_stuff
   	else
+      flash[:danger] = 'Something wrong, please recreate new account'
   		render 'new'
   	end
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = User.get_user_by_id(params[:id])
   end
 
   def update
@@ -38,6 +42,18 @@ class UsersController < ApplicationController
     else
       flash.now[:danger] = 'Cannot update information'
       render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.get_user_by_id(params[:id])
+    dup_user = @user.dup
+    if @user.destroy
+      flash[:success] = 'User has been deleted'
+      redirect_back_or root_url
+    else
+      flash[:danger] = 'Cannot delete user'
+      redirect_back_or root_url
     end
   end
 
@@ -56,7 +72,20 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = current_user
-      unless @user.id == params[:id]
+      unless @user.id != params[:id]
+        flash[:danger] = 'You are not authorized to do this action!'
+        redirect_to root_url
+      end
+    end
+
+    def activate_stuff
+      UserMailer.activate_account(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+    end
+
+    def admin_user
+      @current_user = current_user
+      unless @current_user.admin
         flash[:danger] = 'You are not authorized to do this action!'
         redirect_to root_url
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
 
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token, 
+					:reset_token, :reset_send_at
 
 	before_save {self.email = email.downcase}
+	before_create :create_activation_digest
 
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -21,17 +23,46 @@ class User < ActiveRecord::Base
 		update_attribute(:remember_digest, nil)
 	end
 
+	def activate
+		update_attributes(activated: true, activated_at: Time.zone.now)
+	end
+
+	def authenticated? remember_digest, remember_token
+		BCrypt::Password.new(remember_digest).is_password? remember_token
+	end
+
+	def authenticated? attribute
+		digest = send("#{attribute}_digest")
+		token = send("#{attribute}_token")
+		BCrypt::Password.new(digest).is_password? token
+	end
+
 	def User.digest string
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)														
 	end
 
+	def User.get_user_by_id user_id
+  	begin
+			user = User.find(user_id)
+		rescue
+			nil
+		end
+  end
+
 	def User.new_token
 		SecureRandom.urlsafe_base64
 	end
 
-	def authenticated? remember_digest
-		BCrypt::Password.new(remember_digest).is_password? remember_token
+	def create_password_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_send_at, Time.zone.now)
 	end
+	private
 
+		def create_activation_digest
+			self.activation_token = User.new_token
+			self.activation_digest = User.digest self.activation_token
+		end
 end
